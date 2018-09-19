756865fa168bb306b8dfb39d28b9c168b0cc14bb
changcheng1 <364000100@qq.com>
2018-09-19 11:21:49 +0800

    音乐组件的编写

--- components/music/music.js
+++ components/music/music.js
@@ -12,13 +12,27 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    pauseSrc:'images/播放中.png',
+    playerSrc:'images/暂停中.png',
+    playing:false
   },
-
   /**
    * 组件的方法列表
    */
   methods: {
-
+    playMusic:function(){
+      const backgroundAudioManager = wx.getBackgroundAudioManager()
+      if (this.data.playing){
+        backgroundAudioManager.src = this.properties.src
+        this.setData({
+          playing: false
+        })
+      }else{
+        backgroundAudioManager.pause()
+        this.setData({
+          playing: true
+        })
+      }
+    }
   }
 })
